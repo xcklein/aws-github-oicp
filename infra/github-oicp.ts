765dabd201a64c489bf821bd95854614ac329e83
Christian Klein <xcklein@gmail.com>
2025-09-13 13:03:40 -0500

    update role permissions

--- infra/github-oicp.ts
+++ infra/github-oicp.ts
@@ -2,7 +2,13 @@ import { aws_iam as IAM, Stack } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 
 export interface GithubOicpProps {
+  /**
+   * The github repo owner username.
+   */
   user: string;
+  /**
+   * The github repo slug.
+   */
   repo: string;
 }
 
@@ -19,6 +25,7 @@ export class GithubOicp extends Construct {
     });
 
     this.role = new IAM.Role(this, 'Role', {
+      roleName: 'github-oicp-role',
       assumedBy: new IAM.WebIdentityPrincipal(this.oicp.openIdConnectProviderArn, {
         StringLike: {
           'token.actions.githubusercontent.com:sub': `repo:${props.user}/${props.repo}:*`,
@@ -31,6 +38,27 @@ export class GithubOicp extends Construct {
         effect: IAM.Effect.ALLOW,
         actions: ['ssm:GetParameter'],
         resources: [`arn:aws:ssm:*:${Stack.of(this).account}:parameter/cdk-bootstrap/*`],
+      }),
+    );
+    this.role.addToPolicy(
+      new IAM.PolicyStatement({
+        effect: IAM.Effect.ALLOW,
+        actions: ['cloudformation:*'],
+        resources: ['*'],
+      }),
+    );
+    this.role.addToPolicy(
+      new IAM.PolicyStatement({
+        effect: IAM.Effect.ALLOW,
+        actions: ['s3:*'],
+        resources: ['arn:aws:s3:::cdktoolkit-stagingbucket-*/*'],
+      }),
+    );
+    this.role.addToPolicy(
+      new IAM.PolicyStatement({
+        effect: IAM.Effect.ALLOW,
+        actions: ['sts:AssumeRole'],
+        resources: ['arn:aws:iam::*:role/cdk*'],
       }),
     );
   }
